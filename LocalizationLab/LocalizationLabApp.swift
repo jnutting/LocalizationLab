66b728d78cf460a1ee57e0ab8b3e20f8716f8a96
Jack Nutting <jacknutting@mac.com>
2023-01-31 11:21:16 +0100

    Initial Commit

---
+++ LocalizationLab/LocalizationLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationLabApp.swift
+//  LocalizationLab
+//
+//  Created by JN on 2023-1-31.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
